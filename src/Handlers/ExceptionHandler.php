04e9ad590569f9df6399b9c3e019cbd759bacd6e
hoffinator <csac3539@uibk.ac.at>
2016-10-01 18:37:19 +0200

    Added the unauthenticated method
    The unauthenticated method should be added according to the Laravel Upgrade Guide:
    https://laravel.com/docs/5.3/upgrade#upgrade-5.3.0

--- src/Handlers/ExceptionHandler.php
+++ src/Handlers/ExceptionHandler.php
@@ -137,4 +137,20 @@ class ExceptionHandler extends BaseExceptionHandler
         // For production/non-debug environments, use the PlainDisplay class.
         return $this->app->make(PlainDisplay::class)->setRequest($request)->display($exception, $code);
     }
+    
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest('login');
+    }
 }
